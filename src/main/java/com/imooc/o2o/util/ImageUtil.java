1b0a0da9238f01e3907ec3fcc2107647755506ad
Wei <wesleyoneway@hotmail.com>
2018-08-10 17:12:20 -0400

    ImageUtil completed

--- src/main/java/com/imooc/o2o/util/ImageUtil.java
+++ src/main/java/com/imooc/o2o/util/ImageUtil.java
@@ -2,15 +2,112 @@ package com.imooc.o2o.util;
 
 import net.coobird.thumbnailator.Thumbnails;
 import net.coobird.thumbnailator.geometry.Positions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 
 public class ImageUtil {
 
+    private static String basePath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+    private static final SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+    private static final Random r = new Random();
+    private static Logger logger = LoggerFactory.getLogger(ImageUtil.class);
+
+    /**
+     * transfer CommonsMultipartFile to File
+     * @param cFile
+     * @return
+     */
+    public static File transferCommonsMultipartFileToFile(CommonsMultipartFile cFile){
+
+        File newFile = new File(cFile.getOriginalFilename());
+        try {
+            cFile.transferTo(newFile);
+        }catch (IllegalStateException e){
+            logger.error(e.toString());
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            logger.error(e.toString());
+            e.printStackTrace();
+        }
+        return newFile;
+    }
+
+    /**
+     * deal with thumbnails and return relative address of created picture
+     * @param thumbnail
+     * @param targetAddr
+     * @return
+     */
+    public static String generateThumbnail(File thumbnail, String targetAddr){
+        String realFileName = getRandomFileName();
+        String extension = getFileExtension(thumbnail);
+        makeDirPath(targetAddr);
+        String relativeAddr = targetAddr + realFileName + extension;
+        logger.debug("current relativeAddr is: "+relativeAddr);
+        File dest = new File(PathUtil.getImgBasePath() + relativeAddr);
+        logger.debug("current complete addr is: "+ PathUtil.getImgBasePath() + relativeAddr);
+        try {
+            Thumbnails
+                    .of(thumbnail)
+                    .size(200, 200)
+                    .watermark(Positions.BOTTOM_RIGHT, ImageIO.read(new File(basePath + "/watermark.jpg")),0.25f)
+                    .outputQuality(0.8f)
+                    .toFile(dest);
+        }catch (IOException e){
+            logger.error(e.toString());
+            e.printStackTrace();
+        }
+
+        return relativeAddr;
+    }
+
+    /**
+     * create directory related to target directory:
+     * if the target directory is: /home/work/xiangze/xxx.jpg
+     * we have to create these three folders: home, work, and xiangze
+     * @param targetAddr
+     */
+    private static void makeDirPath(String targetAddr) {
+        String realFileParentPath = PathUtil.getImgBasePath() + targetAddr;
+        File dirPath = new File(realFileParentPath);
+        if (!dirPath.exists()){
+            dirPath.mkdirs();
+        }
+    }
+
+    /**
+     * get the extension name of input file stream
+     * @param cFile
+     * @return
+     */
+    private static String getFileExtension(File cFile) {
+        String originalFileName = cFile.getName();
+        return originalFileName.substring(originalFileName.lastIndexOf("."));
+    }
+
+    /**
+     * generate random file name:
+     * current year month day hour minute second + five-digit random number
+     * @return
+     */
+    private static String getRandomFileName() {
+        //get a five-digit random number
+        int rannum = r.nextInt(89999) + 10000;
+        String nowTimeStr = sDateFormat.format(new Date());
+        return nowTimeStr + rannum;
+    }
+
     public static void main(String[] args) throws IOException {
-        String basePath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+
         Thumbnails
                 .of(new File("/Users/wesley/Pictures/flower.jpg"))
                 .size(200, 200)
