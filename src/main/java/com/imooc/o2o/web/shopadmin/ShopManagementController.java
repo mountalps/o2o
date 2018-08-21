b2de2dee11e856d36bf20cf4eb11f02617013699
Wei <wesleyoneway@hotmail.com>
2018-08-21 16:49:06 -0400

    get shop by id

--- src/main/java/com/imooc/o2o/web/shopadmin/ShopManagementController.java
+++ src/main/java/com/imooc/o2o/web/shopadmin/ShopManagementController.java
@@ -41,6 +41,33 @@ public class ShopManagementController {
     @Autowired
     private AreaService areaService;
 
+    @RequestMapping(value = "getshopbyid", method = RequestMethod.GET)
+    @ResponseBody
+    private Map<String, Object> getShopById(HttpServletRequest request){
+        Map<String, Object> modelMap = new HashMap<>();
+        Long shopId = HttpServletRequestUtil.getLong(request, "shopId");
+
+        if (shopId > -1){
+
+            try{
+                Shop shop = shopService.getByShopId(shopId);
+                List<Area> areaList = areaService.getAreaList();
+                modelMap.put("shop", shop);
+                modelMap.put("areaList", areaList);
+                modelMap.put("success", true);
+            }catch (Exception e){
+                modelMap.put("success", false);
+                modelMap.put("errMsg", e.toString());
+            }
+
+        }else {
+            modelMap.put("success",false);
+            modelMap.put("errMsg", "empty shopId");
+        }
+
+        return modelMap;
+    }
+
     @RequestMapping(value = "/getshopinitinfo", method = {RequestMethod.GET})
     @ResponseBody//transfer return parameter to JSON
     private Map<String, Object> getShopInitInfo(){
@@ -105,14 +132,22 @@ public class ShopManagementController {
         //2. register shop
         if (shop != null && shopImg != null){
             //use session later
-            PersonInfo owner = new PersonInfo();
-            owner.setUserId(1L);
+            PersonInfo owner = (PersonInfo)request.getSession().getAttribute("user");
             shop.setOwner(owner);
             ShopExecution se = null;
             try {
                 se = shopService.addShop(shop, shopImg.getInputStream(), shopImg.getOriginalFilename());
                 if (se.getState() == ShopStateEnum.CHECK.getState()){
                     modelMap.put("success", true);
+                    //all shops that belongs to this owner
+                    @SuppressWarnings("unchecked")
+                    List<Shop> shopList = (List<Shop> )request.getSession().getAttribute("shopList");
+                    if (shopList == null || shopList.size() == 0){
+                        shopList = new ArrayList<Shop>();
+                    }
+                    shopList.add(se.getShop());
+                    request.getSession().setAttribute("shopList", shopList);
+
                 }
                 else {
                     modelMap.put("success", false);
@@ -131,6 +166,84 @@ public class ShopManagementController {
         }else {
             modelMap.put("success", false);
             modelMap.put("errMsg", "please input shop information ");
+            return modelMap;
+        }
+
+
+    }
+
+    /**
+     *
+     * @param request, includes all information in http request
+     * @return necessary key-value
+     */
+    @RequestMapping(value = "/modifyshop", method = RequestMethod.POST)
+    @ResponseBody
+    private Map<String, Object> modifyShop(HttpServletRequest request){
+        Map<String ,Object> modelMap = new HashMap<>();
+
+        if (!CodeUtil.checkVerifyCode(request)){
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "wrong verify code!");
+            return modelMap;
+        }
+
+        //1. receive and transfer parameter, including shop information and picture information
+        //1.1 this is to deal with shop information from http request
+        String shopStr = HttpServletRequestUtil.getString(request, "shopStr");
+        //deserialize JSON string into Java objects
+        ObjectMapper mapper = new ObjectMapper();
+        Shop shop = null;
+        try {
+            shop = mapper.readValue(shopStr, Shop.class);
+        }catch (Exception e){
+            modelMap.put("success", false);
+            modelMap.put("errMsg", e.getMessage());
+            return modelMap;
+        }
+
+        //1.2 this is to deal with file from http request
+        CommonsMultipartFile shopImg = null;
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
+        if (commonsMultipartResolver.isMultipart(request)){
+            MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest)request;
+            shopImg = (CommonsMultipartFile)multipartHttpServletRequest.getFile("shopImg");
+        }
+
+        //2. modify shop
+        if (shop != null && shop.getShopId() != null){
+            //use session later
+
+            ShopExecution se = null;
+            try {
+
+                if (shopImg == null){
+                    se = shopService.modifyShop(shop, null, shopImg.getOriginalFilename());
+                }
+                else {
+                    se = shopService.modifyShop(shop, shopImg.getInputStream(), shopImg.getOriginalFilename());
+                }
+
+                if (se.getState() == ShopStateEnum.SUCCESS.getState()){
+                    modelMap.put("success", true);
+                }
+                else {
+                    modelMap.put("success", false);
+                    modelMap.put("errMsg", se.getStateInfo());
+                }
+            } catch (IOException e) {
+                modelMap.put("success", false);
+                modelMap.put("errMsg", se.getStateInfo());
+            } catch (ShopOperationException e){
+                modelMap.put("success", false);
+                modelMap.put("errMsg", se.getStateInfo());
+            }
+
+            //3. return result(several returns)
+            return modelMap;
+        }else {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "please input shopid");
             return modelMap;
         }
 
--- src/main/webapp/resources/js/shop/shopoperation.js
+++ src/main/webapp/resources/js/shop/shopoperation.js
@@ -5,10 +5,50 @@
  */
 
 $(function () {
+    var shopId = getQueryString('shopId');
+    var isEdit = shopId? true:false;
     var initUrl = '/shopadmin/getshopinitinfo';
     var registerShopUrl = '/shopadmin/registershop';
-    // alert(initUrl);
-    getShopInitInfo();
+    var shopInfoUrl = '/shopadmin/getshopbyid?shopId='+shopId;
+    var editShopUrl = '/shopadmin/modifyshop';
+    
+    if (!isEdit) {
+        getShopInitInfo();
+    }else {
+        getShopInfo(shopId);
+    }
+
+
+    // 通过店铺Id获取店铺信息
+    function getShopInfo(shopId) {
+        $.getJSON(shopInfoUrl, function(data) {
+            if (data.success) {
+                // 若访问成功，则依据后台传递过来的店铺信息为表单元素赋值
+                var shop = data.shop;
+                $('#shop-name').val(shop.shopName);
+                $('#shop-addr').val(shop.shopAddr);
+                $('#shop-phone').val(shop.phone);
+                $('#shop-desc').val(shop.shopDesc);
+                // 给店铺类别选定原先的店铺类别值
+                var shopCategory = '<option data-id="'
+                    + shop.shopCategory.shopCategoryId + '" selected>'
+                    + shop.shopCategory.shopCategoryName + '</option>';
+                var tempAreaHtml = '';
+                // 初始化区域列表
+                data.areaList.map(function(item, index) {
+                    tempAreaHtml += '<option data-id="' + item.areaId + '">'
+                        + item.areaName + '</option>';
+                });
+                $('#shop-category').html(shopCategory);
+                // 不允许选择店铺类别
+                $('#shop-category').attr('disabled', 'disabled');
+                $('#area').html(tempAreaHtml);
+                // 给店铺选定原先的所属的区域
+                $("#area option[data-id='" + shop.area.areaId + "']").attr(
+                    "selected", "selected");
+            }
+        });
+    }
 
     function getShopInitInfo() {
         //get shopcategory and area into shopoperation.html
@@ -39,6 +79,9 @@ $(function () {
 
     $('#submit').click(function () {
         var shop = {};
+        if (isEdit){
+            shop.shopId = shopId;
+        }
         shop.shopName = $('#shop-name').val();
         shop.shopAddr = $('#shop-addr').val();
         shop.phone = $('#shop-phone').val();
@@ -78,7 +121,7 @@ $(function () {
 
 
         $.ajax({
-            url: registerShopUrl,
+            url: (isEdit? editShopUrl : registerShopUrl),
             type: 'POST',
             data: formData,
             contentType: false,
